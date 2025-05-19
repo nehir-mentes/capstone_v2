037ab0d6e9aa5b650b8880ae74de274599fd0168
nehir-mentes <nehirmen.98@gmail.com>
2025-05-19 18:22:24 +0000

    devise installed and migrated

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :users
 
-  # This is a blank app! Pick your first screen, build out the RCAV, and go from there. E.g.:
-
-  # get "/your_first_screen" => "pages#first"
+root "sessions#index"
   
 end
